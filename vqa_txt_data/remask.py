757687f54095d59e95693e18df77ce38f1e348fd
William Macke <wmacke@cs.utexas.edu>
2021-04-19 13:46:57 -0500

    made file more use friendly

--- vqa_txt_data/remask.py
+++ vqa_txt_data/remask.py
@@ -7,37 +7,40 @@ from lz4.frame import compress, decompress
 
 
 
-MASK = "@C"
+MASK = "[MASK]"
 
 
-f_name = sys.argv[1]
 
-with open('vqa_words_not_in_bert.txt', 'r') as f:
-    words = json.load(f)
+def remask(f_name, vocab_loc='vqa_words_not_in_bert.txt'):
+    with open(vocab_loc, 'r') as f:
+        words = json.load(f)
+    
+    tok = BertTokenizer.from_pretrained('bert-base-cased')
+    
+    
+    env = lmdb.open(f_name, readonly=True, create=False, map_size=4*1024**4)
+    txn = env.begin()
+    cursor = txn.cursor()
+    
+    tokenized_queries = {}
+    
+    for key, value in cursor:
+        q_id = key.decode()
+        q = msgpack.loads(decompress(value))
+        query_text = q['question']
+        if query_text[-1] == '?':
+            query_text = query_text[0:-1]
+        query_words = query_text.split(" ")
+        query_tokens = []
+        for i, word in enumerate(query_words):
+            if word in words:
+                query_tokens += [MASK]
+            else:
+                query_tokens += tok.tokenize(word)
+        q['new_toked_question'] = query_tokens
+        tokenized_queries[q_id] = q
+    return tokenized_queries
 
-tok = BertTokenizer.from_pretrained('bert-base-cased')
-
-
-env = lmdb.open(f_name, readonly=True, create=False, map_size=4*1024**4)
-txn = env.begin()
-cursor = txn.cursor()
-
-tokenized_queries = {}
-
-for key, value in cursor:
-    q_id = key.decode()
-    q = msgpack.loads(decompress(value))
-    query_text = q['question']
-    if query_text[-1] == '?':
-        query_text = query_text[0:-1]
-    query_words = query_text.split(" ")
-    query_tokens = []
-    for i, word in enumerate(query_words):
-        if word in words:
-            query_tokens += [MASK]
-        else:
-            query_tokens += tok.tokenize(word)
-    q['new_toked_question'] = query_tokens
-    tokenized_queries[q_id] = q
-
-print(tokenized_queries)
+if __name__ == '__main__':
+    tokenized_queries = remask(sys.argv[1])
+    print(tokenized_queries)
