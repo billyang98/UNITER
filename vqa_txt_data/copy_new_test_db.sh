8c3de7482018b67bdbe76d4b022d21f8b9aff653
Bill Yang <billyang@eldar-11.cs.utexas.edu>
2021-04-20 02:46:24 +0000

    ran eval on the masked dataset

--- vqa_txt_data/copy_new_test_db.sh
+++ vqa_txt_data/copy_new_test_db.sh
@@ -1,7 +1,7 @@
 USERNAME=$1
 OUTDIR=$2
 
-cp /scratch/cluster/$USERNAME/vqa_dataset/txt_db/oov_datasets/test_set.db/img2txts.json $OUTDIR/img2txts.json
-cp /scratch/cluster/$USERNAME/vqa_dataset/txt_db/oov_datasets/test_set.db/txt2img.json $OUTDIR/txt2img.json
-cp /scratch/cluster/$USERNAME/vqa_dataset/txt_db/oov_datasets/test_set.db/meta.json $OUTDIR/meta.json
+cp /scratch/cluster/$USERNAME/vqa_dataset/txt_db/oov_datasets/oov_test_set.db/img2txts.json $OUTDIR/img2txts.json
+cp /scratch/cluster/$USERNAME/vqa_dataset/txt_db/oov_datasets/oov_test_set.db/txt2img.json $OUTDIR/txt2img.json
+cp /scratch/cluster/$USERNAME/vqa_dataset/txt_db/oov_datasets/oov_test_set.db/meta.json $OUTDIR/meta.json
 
