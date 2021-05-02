fb75f719c8b6678329ff2a88a513aa0a761e62e6
Bill Yang <bill@Bills-MBP.attlocal.net>
2021-05-01 22:38:25 -0500

    add more analysis files

---
+++ questions_answers/make_qam_csv.py
@@ -0,0 +1,19 @@
+import json
+import csv
+
+
+qas_file_names = ['qas_ms_2_pg04.json']
+
+for qas_fname in qas_file_names:
+    qas_json = json.load(open(qas_fname))
+    list_names = ['wtr', 'rtw']
+    for l in list_names:
+        csv_lists = []
+        qs_dict = qas_json[l]
+        for qid, q in qs_dict.items():
+            csv_row_list = [qid, q['img_fname'], q['bl_q'], q['bl_ans'], q['mexp_q'], q['mexp_ans'], q['exp_q'], q['exp_ans']]
+            csv_lists.append(csv_row_list)
+
+        with open('{}_{}.csv'.format(qas_fname[:-len('.json')], l), 'w') as f: 
+            write = csv.writer(f)
+            write.writerows(csv_lists)
